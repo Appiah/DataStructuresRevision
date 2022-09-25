5f93b1f88651f408519084eaf86647348b17a989
Appiah <emap218@gmail.com>
2022-09-25 21:22:43 +0000

    Basic addition to an ArrayList; add(item), add(index_x, item)

--- src/List/List_ArrayList.java
+++ src/List/List_ArrayList.java
@@ -11,8 +11,25 @@ public class List_ArrayList {
         System.out.print("Data Structures Experimentation with : ");
         System.out.print(DATA_STRUCTURE_NAME);
 
+        System.out.println("");
+
         ArrayList<String> arrList = new ArrayList<>();
+
+        //adding of new items : O(1)
         arrList.add("Item 00");
+        arrList.add("Item 01");
+        arrList.add("Item 01");
+        arrList.add("Item 02");
+
+        //adding of a new item at a particular index
+        arrList.add(2, "Item 03");
+        //arrList.add(7, "Item 09");//this would yield this error :
+        /*
+        * Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 7, Size: 5
+        * */
+
+        System.out.println("arrList elements : "+arrList.toString());
+
 
     }
 
